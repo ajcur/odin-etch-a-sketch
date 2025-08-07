7a5cf90f43b2f7dae72e1a9cd4af9d182f23de03
ajcur <173679835+ajcur@users.noreply.github.com>
2025-08-07 19:04:24 +0200

    first commit

---
+++ javascript.js
@@ -0,0 +1,22 @@
+function createGrid(gridNumber) {
+    let container = document.querySelector("#container");
+    for (let i = 0; i < gridNumber; i++) {
+        let gridRow = document.createElement("div");
+        for (let j = 0; j < gridNumber; j++) {
+            let gridSquare = document.createElement("div");
+            if (window.innerWidth < window.innerHeight) {
+                squareWidth = (window.innerWidth / gridNumber) * 0.95;
+            } else {
+                squareWidth = (window.innerHeight / gridNumber) * 0.95;
+            }
+            gridSquare.style.boxSizing = "border-box";
+            gridSquare.style.width = squareWidth + "px";
+            gridSquare.style.height = squareWidth + "px";
+            gridSquare.style.border = "1px solid black";
+            gridRow.appendChild(gridSquare);
+        }
+        container.appendChild(gridRow);
+    }
+}
+
+createGrid(5);
